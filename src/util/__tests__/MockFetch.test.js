e1c8b7f9708a01a7805beb2f02bd5a197ad1e4e9
Eugene Krylov <eugene@mabl.com>
2019-10-03 12:57:57 -0400

    Added example of mocking with regex.

--- src/util/__tests__/MockFetch.test.js
+++ src/util/__tests__/MockFetch.test.js
@@ -15,6 +15,14 @@ describe('Mock fetch', () => {
     expect(actualResponse).to.eql(mockData);
   });
 
+  it('Mock calls matching regex', async () => {
+    const mockData = {bacon: 1};
+    fetchMock.mock(/firebaseio\.com\/ingredients\.json/, mockData);
+
+    const actualResponse = await fetchIngredients();
+    expect(actualResponse).to.eql(mockData);
+  });
+
   it('Mock failure case', async () => {
     const MY_ERROR = 'my error';
     fetchMock.mock('*', Promise.reject(MY_ERROR));
